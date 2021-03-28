7cd0f2514fd478b6016b10bbbec1ef6c42fd9ddb
Balogun Silver <silverg33k@gmail.com>
2021-03-28 05:14:52 +0100

    Openzeppelin contracts version changed from 4.0.0 to 3.4.0... more stable and compiles

--- contracts/violenceFC.sol
+++ contracts/violenceFC.sol
@@ -1,6 +1,24 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol"
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract violenceFC is ERC721{
-  constructor() public ERC721("violenceFC", "VFC") {}
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    mapping(string => uint8) hashes;
+
+    constructor() public ERC721("violenceFC", "VFC") {}
+
+    function awardItem(address recipient, string memory hash, string memory metadata) 
+    public returns (uint256) {
+            require(hashes[hash] != 1);
+            hashes[hash] = 1;
+            _tokenIds.increment();
+            uint256 newItemId = _tokenIds.current();
+            _mint(recipient, newItemId);
+            _setTokenURI(newItemId, metadata);
+            return newItemId;
+        }
 }
